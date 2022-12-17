71da435bd0c29861b774a22593caa5c797ade49c
Nicholas <razvan.botorog@yahoo.com>
2022-12-17 18:30:14 -0500

    index page

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,9 +1,44 @@
+import "./categories.style.scss"
 
 
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    },
+  ]
+
   return (
-    <div>
-      Hello World
+    <div className="categories-container">
+
+      {categories.map(({title, id}) => ( 
+        <div key = {id} className="category-container">
+        <div className="background-image"/>
+        <div className="category-body-container">
+          <h2>{title}</h2>
+          <p>Buy Now</p>
+        </div>
+      </div>
+      ))}
+
     </div>
   );
 }
